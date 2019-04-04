d2ececd50c82620b06d6dc6c08394a77ee20dbc3
lucas <lucas.machado.gutierrez@gmail.com>
2019-04-04 10:31:33 -0300

    vers1

---
+++ func.R
@@ -0,0 +1,19 @@
+conf.interval2 <- function(x, s, n, conf.level=0.95) {
+	alfa = 1 - conf.level
+	if (n >= 30) {
+		z = qnorm(1 - alfa/2)
+	}
+	else {
+		z = qt(1 - alfa/2, n-1)
+	}
+
+	return(c(x - z * s / sqrt(n), x + z * s / sqrt(n)))
+}
+
+conf.interval <- function(x, conf.level=0.95) {
+	return(conf.interval2(mean(x), sd(x), length(x), conf.level))
+}
+
+cv <- function(x) {
+	return (sd(x) / mean(x))
+}
